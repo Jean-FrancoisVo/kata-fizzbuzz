d0dd0aecccb46eeee35f1487ec4605548dfacb81
JeanFrancoisVo <jeanfrancoisvo7@gmail.com>
2018-05-25 12:12:13 +0200

    Second step : Given a number divisable by 3 then return fizz and Given a number divisable by 5 then return buzz

--- src/test/kotlin/fr/lacombe/kata/FizzBuzzTest.kt
+++ src/test/kotlin/fr/lacombe/kata/FizzBuzzTest.kt
@@ -5,11 +5,28 @@ import org.junit.jupiter.api.Test
 
 class FizzBuzzTest {
     @Test
-    fun `when number then return the number`() {
+    fun `Given one number then return the number`() {
+        //TODO Parameterized test
         assertThat(fizzbuzz(1)).isEqualTo("1")
     }
 
+    @Test
+    fun `Given a number divisable by 3 then return 3`() {
+        assertThat(fizzbuzz(3)).isEqualTo("fizz")
+    }
+
+    @Test
+    fun `Given a number divisable by 5 then return 5`() {
+        assertThat(fizzbuzz(5)).isEqualTo("buzz")
+    }
+
     private fun fizzbuzz(value: Int): String {
+        if (value % 3 == 0) {
+            return "fizz"
+        }
+        if (value % 5 == 0) {
+            return "buzz"
+        }
         return Integer.toString(value)
     }
 }
